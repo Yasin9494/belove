4ed868d728f1f95e6e56ea000787fe3825ad9fef
Ясин <yasin_94@mail.ru>
2023-04-01 21:28:09 +0500

    Delete opentimetable directory

--- opentimetable/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$dbhost = "localhost";
-	$dbname = "timetable";
-	$dbuser = "timetable";
-	$dbpasswd = "";
-	$link = mysql_connect($dbhost,$dbuser,$dbpasswd) or die(mysql_error()); 
-	mysql_select_db($dbname,$link)  or die(mysql_error());
-?>
